f115b4e3a11e3320e1ae16bdd75d94987c677f99
LilBakso <egedi.rikardo@gmail.com>
2023-05-19 15:13:38 +0200

    damageOverTime() implementálása a Fight() metódusba

--- src/com/company/Battlefield.java
+++ src/com/company/Battlefield.java
@@ -2,46 +2,49 @@ package com.company;
 
 public class Battlefield {
     private Fighter fighter;
-    private Player player2;
+    private Wizard wizard;
 
-    public Battlefield(Fighter fighter, Player player2){
+    public Battlefield(Fighter fighter, Wizard wizard){
         this.fighter = fighter;
-        this.player2 = player2;
+        this.wizard = wizard;
     }
 
     public void Move(){
         fighter.Move();
-        player2.Move();
+        wizard.Move();
     }
 
     public void Fight(){
-        if (fighter.isFighting(player2)){
+        if (fighter.isFighting(wizard)){
             System.out.println("Harc!");
             if (!fighter.isBlocking()){
-                player2.Damage(fighter);
+                wizard.Damage(fighter);
             }
-            fighter.Damage(player2);
+            fighter.Damage(wizard);
+        }
+        else{
+            System.out.println("ÉGETÉS!!!");
+            wizard.damageOverTime(fighter);
         }
     }
 
-    public static String buildField(int fieldsize,Player player1, Player player2){
+    public static String buildField(int fieldsize,Fighter fighter, Wizard wizard){
         StringBuilder battlefield = new StringBuilder();
 
         for (int i = 0; i < fieldsize; i++){
-            if(i == player1.getPosition() && i == player2.getPosition()) battlefield.append("X");
-            else if (i == player1.getPosition()) battlefield.append("F");
-            else if (i == player2.getPosition()) battlefield.append("W");
+            if(i == fighter.getPosition() && i == wizard.getPosition()) battlefield.append("X");
+            else if (i == fighter.getPosition()) battlefield.append("F");
+            else if (i == wizard.getPosition()) battlefield.append("W");
             else battlefield.append("_");
         }
-        battlefield.append("-->\t"+player1.getName()+" élete:"+player1.getHealth()+"\t"+player2.getName()+" élete:"+player2.getHealth());
+        battlefield.append("-->\t"+fighter.getName()+" élete:"+fighter.getHealth()+"\t"+wizard.getName()+" élete:"+wizard.getHealth());
         return  battlefield.toString();
     }
 
-    public void result(Player player1, Player player2){
-        if(player1.getHealth()<=0 && player2.getHealth() <= 0) System.out.println("Mindkét karakter meghalt!");
-        else if (player1.getHealth() > 0 && player2.getHealth() < 0) System.out.println("Harcos nyert!");
-        else if (player1.getHealth() < 0 && player2.getHealth() > 0) System.out.println("Varázsló nyert!");
+    public void result(Fighter fighter, Wizard wizard){
+        if(fighter.getHealth()<=0 && wizard.getHealth() <= 0) System.out.println("Mindkét karakter meghalt!");
+        else if (fighter.getHealth() > 0 && wizard.getHealth() <= 0) System.out.println("Harcos nyert!");
+        else if (fighter.getHealth() <= 0 && wizard.getHealth() > 0) System.out.println("Varázsló nyert!");
         else System.out.println("A harc folytatódik\n");
-
     }
 }

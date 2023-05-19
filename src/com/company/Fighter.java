71a7bf6310aa7e31594d8c257245a2c9e6a4fc41
LilBakso <egedi.rikardo@gmail.com>
2023-05-19 15:14:53 +0200

    getHealth() törlése

--- src/com/company/Fighter.java
+++ src/com/company/Fighter.java
@@ -4,23 +4,18 @@ public class Fighter extends Player {
 
     public Fighter(String name) {
         super(name, new Dice(6), (int)(Math.random()*3));
-        this.setHealth(d6.Throw()+ d6.Throw());
+        this.setHealth(dice.Throw()+ dice.Throw());
     }
 
     public boolean isBlocking(){
-        int block = d6.Throw();
-            if (block > 5) {
-                System.out.println("Harcos blokkolta az ütést!");
+        int block = dice.Throw();
+            if (block > 3) {
+                System.out.println("Harcos blokkolta az ütést");
                 return true;
             }
             else{
-                System.out.println("Harcos nem tudta blokkolni az ütést");
+                System.out.println("Harcos nem blokkolta az ütést");
                 return false;
             }
-    }
-
-    @Override
-    public int getHealth() {
-        return health;
     }
 }

561a8dbf6b6f0e8f76ebc40d14ab0573d12e9035
LilBakso <egedi.rikardo@gmail.com>
2023-05-19 15:45:09 +0200

    Gyermek osztály és öröklődés teljes!

--- src/com/company/Fighter.java
+++ src/com/company/Fighter.java
@@ -5,6 +5,7 @@ public class Fighter extends Player {
     public Fighter(String name) {
         super(name, new Dice(6), (int)(Math.random()*3));
         this.setHealth(dice.Throw()+ dice.Throw());
+
     }
 
     public boolean isBlocking(){
--- src/com/company/Wizard.java
+++ src/com/company/Wizard.java
@@ -5,6 +5,7 @@ public class Wizard extends Player{
     public Wizard(String name) {
         super(name, new Dice(4), (int)(Math.random()*3));
         this.setHealth(dice.Throw());
+
     }
     public void damageOverTime(Player enemy){
             enemy.setHealth(enemy.getHealth()-1);
